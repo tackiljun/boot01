c9d712e73fb5678a6774a4219084248db70cafcb
tarkIlJun <tack89826@gmail.com>
2023-09-11 20:18:48 +0900

    TodoRepository

--- src/main/java/tack/project/boot01/repository/TodoRepository.java
+++ src/main/java/tack/project/boot01/repository/TodoRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import tack.project.boot01.domain.Todo;
 
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-    
+
 }
