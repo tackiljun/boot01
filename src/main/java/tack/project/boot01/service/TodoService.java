70aac37315d1519fb5bd3bd436259f8581d8fd83
tarkIlJun <tack89826@gmail.com>
2023-09-11 17:42:58 +0900

    TodoService

--- src/main/java/tack/project/boot01/service/TodoService.java
+++ src/main/java/tack/project/boot01/service/TodoService.java
@@ -1,12 +1,17 @@
 package tack.project.boot01.service;
 
 import jakarta.transaction.Transactional;
+import tack.project.boot01.dto.PageResponseDTO;
 import tack.project.boot01.dto.TodoDTO;
 
 
 @Transactional
 public interface TodoService {
     
+    ////////////////////////////////////
+    PageResponseDTO<TodoDTO> getList();
+
+    ////////////////////////////////////
     TodoDTO register(TodoDTO dto);
 
     TodoDTO getOne(Long tno);
