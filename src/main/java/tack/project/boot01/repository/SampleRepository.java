f6d6b451765f003b94199edc833703567fb5576b
tarkIlJun <tack89826@gmail.com>
2023-09-12 15:51:35 +0900

    SampleRepository

--- src/main/java/tack/project/boot01/repository/SampleRepository.java
+++ src/main/java/tack/project/boot01/repository/SampleRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import tack.project.boot01.domain.Sample;
 
+
 public interface SampleRepository extends JpaRepository<Sample, String> {
     
 }
