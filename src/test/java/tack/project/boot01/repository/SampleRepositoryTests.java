f1367de7931197da95b62bf1398bae8c3d4e9c88
tarkIlJun <tack89826@gmail.com>
2023-09-12 15:52:06 +0900

    SampleRepositoryTests

--- src/test/java/tack/project/boot01/repository/SampleRepositoryTests.java
+++ src/test/java/tack/project/boot01/repository/SampleRepositoryTests.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import lombok.extern.log4j.Log4j2;
 import tack.project.boot01.domain.Sample;
 
+
 @SpringBootTest
 @Log4j2
 public class SampleRepositoryTests {
