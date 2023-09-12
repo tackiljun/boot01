104593116d0b179a453e322193137605b3c2c579
tarkIlJun <tack89826@gmail.com>
2023-09-12 17:01:37 +0900

    BoardService

---
+++ src/main/java/tack/project/boot01/service/BoardService.java
@@ -0,0 +1,17 @@
+package tack.project.boot01.service;
+
+import jakarta.transaction.Transactional;
+import tack.project.boot01.dto.BoardDTO;
+import tack.project.boot01.dto.BoardListRcntDTO;
+import tack.project.boot01.dto.PageRequestDTO;
+import tack.project.boot01.dto.PageResponseDTO;
+
+
+@Transactional
+public interface BoardService {
+
+    PageResponseDTO<BoardListRcntDTO> listRcnt (PageRequestDTO pageRequestDTO);
+
+    BoardDTO getOne(Long bno);
+    
+}
---
+++ src/main/java/tack/project/boot01/service/BoardServiceImpl.java
@@ -0,0 +1,52 @@
+package tack.project.boot01.service;
+
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import tack.project.boot01.domain.Board;
+import tack.project.boot01.dto.BoardDTO;
+import tack.project.boot01.dto.BoardListRcntDTO;
+import tack.project.boot01.dto.PageRequestDTO;
+import tack.project.boot01.dto.PageResponseDTO;
+import tack.project.boot01.repository.BoardRepository;
+
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    private final BoardRepository boardRepository;
+    private final ModelMapper modelMapper;
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public PageResponseDTO<BoardListRcntDTO> listRcnt(PageRequestDTO pageRequestDTO) {
+        
+        log.info("==============================");
+        log.info(pageRequestDTO);
+
+        return boardRepository.searchDTORcnt(pageRequestDTO);
+
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////
+    @Override
+    public BoardDTO getOne(Long bno) {
+        
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board board = result.orElseThrow();
+
+        BoardDTO dto = modelMapper.map(board, BoardDTO.class);
+
+        return dto;
+
+    }
+    
+}
