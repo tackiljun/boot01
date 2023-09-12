48ff4e795451e901e8d1639a1af0b0a76290d43b
tarkIlJun <tack89826@gmail.com>
2023-09-12 17:23:14 +0900

    BoardController, BoardRepositoryTests

---
+++ src/main/java/tack/project/boot01/controller/BoardController.java
@@ -0,0 +1,47 @@
+package tack.project.boot01.controller;
+
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import tack.project.boot01.dto.BoardDTO;
+import tack.project.boot01.dto.BoardListRcntDTO;
+import tack.project.boot01.dto.PageRequestDTO;
+import tack.project.boot01.dto.PageResponseDTO;
+import tack.project.boot01.service.BoardService;
+
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/board")
+@RequiredArgsConstructor
+@Log4j2
+public class BoardController {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    private final BoardService boardService;
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    @GetMapping(value = "/list")
+    public PageResponseDTO<BoardListRcntDTO> list(@ParameterObject PageRequestDTO requestDTO) {
+
+        log.info(requestDTO);
+
+        return boardService.listRcnt(requestDTO);
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////
+    @GetMapping("/{bno}")
+    public BoardDTO get(@PathVariable("bno") Long bno) {
+
+        return boardService.getOne(bno);
+
+    }
+    
+}
---
+++ src/test/java/tack/project/boot01/repository/BoardRepositoryTests.java
@@ -0,0 +1,238 @@
+package tack.project.boot01.repository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.log4j.Log4j2;
+import tack.project.boot01.domain.Board;
+import tack.project.boot01.dto.BoardListRcntDTO;
+import tack.project.boot01.dto.BoardReadDTO;
+import tack.project.boot01.dto.PageRequestDTO;
+import tack.project.boot01.dto.PageResponseDTO;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Autowired
+    private BoardRepository boardRepository;
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test  // 시간값 생성 확인.
+    public void testInsert() {
+
+        for(int i = 0; i < 100; i++) {
+
+            Board board = Board.builder()
+            .title("Sample title" + i)
+            .contents("Sample Contents" + i)
+            .writer("User" + (i %10))
+            .build();
+
+            boardRepository.save(board);
+
+            }
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test  // 조회.
+    public void testRead() {
+
+        Long bno = 1L;
+
+        // Optional null대신 사용.
+        Optional<Board> result = boardRepository.findById(1L);
+
+        log.info("====================");
+        Board board = result.orElseThrow();
+
+        log.info(result.get());
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test  // 업데이트.
+    public void testUpdate() {
+
+        Optional<Board> result = boardRepository.findById(1L);
+
+
+        Board board = result.orElseThrow();
+
+        board.changeTitle("Update Title");
+
+        boardRepository.save(board);
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testQuery1() {
+
+        java.util.List<Board> list = boardRepository.findByTitleContaining("1");
+
+        log.info("====================");
+        log.info(list.size());
+        log.info(list);
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testQuery1_1() {
+
+        java.util.List<Board> list = boardRepository.listTitle("1");
+
+        log.info("====================");
+        log.info(list.size());
+        log.info(list);
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testQuery1_2() {
+
+        java.util.List<Object[]> list = boardRepository.listTitle2("1");
+
+        log.info("====================");
+        log.info(list.size());
+
+        list.forEach(arr -> log.info(Arrays.toString(arr)));
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testQuery1_3() {
+
+        Pageable pageable = 
+            PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Object[]> result = boardRepository.listTitle2("1", pageable);
+
+        log.info(result);
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Commit
+    @Transactional
+    @Test
+    public void testModify() {
+
+        Long bno = 100L;
+        String title = "Modified Title 100";
+
+        int count = boardRepository.modifyTitle(title, bno);
+
+        log.info("====================" + count);
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testNative() {
+
+        List<Object[]> result = boardRepository.listNative();
+
+        result.forEach(arr -> log.info(Arrays.toString(arr)));
+
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testQuery2() {
+
+        Pageable pageable = 
+            PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        Page<Board> result = 
+            boardRepository.findByContentsContaining("1", pageable);
+
+        log.info("====================");
+        log.info(result);
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testSearch1() {
+
+        Pageable pageable = 
+            PageRequest.of(0, 10, Sort.by("bno").descending());
+        
+        Page<Board> result = 
+            boardRepository.search1(null, "1", pageable);
+
+        log.info(result.getTotalElements());
+
+        result.get().forEach(b -> log.info(b));
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testListWithRcnt() {
+
+        List<Object[]> result = boardRepository.getListWithRcnt();
+
+        for (Object[] result2 : result) {
+            log.info(Arrays.toString(result2));    
+        }
+
+    }
+
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testListWithRcntSearch() {
+
+        Pageable pageable = 
+            PageRequest.of(0, 10, Sort.by("bno").descending());
+
+        boardRepository.searchWithRcnt("tcw", "1", pageable);
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void test0912_1() {
+
+        PageRequestDTO pageRequest = new PageRequestDTO();
+
+        PageResponseDTO<BoardListRcntDTO> responseDTO = 
+            boardRepository.searchDTORcnt(pageRequest);
+        
+            log.info(responseDTO);
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////
+    @Test
+    public void testReadOne() {
+        Long bno = 77L;
+
+        BoardReadDTO dto = boardRepository.readOne(bno);
+        log.info(dto);
+        log.info(dto.getRegDate());
+        log.info(dto.getModDate());
+        log.info(dto.getClass().getName());
+
+    }
+    
+}
