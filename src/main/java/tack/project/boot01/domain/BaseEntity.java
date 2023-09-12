6486fa726efd6890c06a715c3938a50d00b6ac36
tarkIlJun <tack89826@gmail.com>
2023-09-12 15:14:09 +0900

    BaseEntity-domain

--- src/main/java/tack/project/boot01/domain/BaseEntity.java
+++ src/main/java/tack/project/boot01/domain/BaseEntity.java
@@ -1,5 +1,28 @@
 package tack.project.boot01.domain;
 
-public class BaseEntity {
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(value = { AuditingEntityListener.class })
+public abstract class BaseEntity {
+
+    //////////////////////////////
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime regDate;
+
+    //////////////////////////////
+    @LastModifiedDate
+    private LocalDateTime modDate;
     
 }
