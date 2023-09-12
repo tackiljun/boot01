5ece9032814da21c8cb3520b96eafefcf8d8857d
tarkIlJun <tack89826@gmail.com>
2023-09-12 19:07:27 +0900

    BoardSearchImpl

--- src/main/java/tack/project/boot01/repository/search/BoardSearchImpl.java
+++ src/main/java/tack/project/boot01/repository/search/BoardSearchImpl.java
@@ -1,53 +1,6 @@
 package tack.project.boot01.repository.search;
 
-import java.util.Optional;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import tack.project.boot01.domain.Board;
-import tack.project.boot01.dto.BoardDTO;
-import tack.project.boot01.dto.BoardListRcntDTO;
-import tack.project.boot01.dto.PageRequestDTO;
-import tack.project.boot01.dto.PageResponseDTO;
-import tack.project.boot01.repository.BoardRepository;
-import tack.project.boot01.service.BoardService;
-
-
-@Service
-@Log4j2
-@RequiredArgsConstructor
-public class BoardSearchImpl implements BoardService {
-
-    ///////////////////////////////////////////////////////////////////////////////////
-    private final BoardRepository boardRepository;
-    private final ModelMapper modelMapper;
-
-    ///////////////////////////////////////////////////////////////////////////////////
-    @Override
-    public PageResponseDTO<BoardListRcntDTO> listRcnt(PageRequestDTO pageRequestDTO) {
-        
-        log.info("--------------------");
-        log.info(pageRequestDTO);
-
-        return boardRepository.searchDTORcnt(pageRequestDTO);
-
-    }
-
-    ///////////////////////////////////////////////////////////////////////////////////
-    @Override
-    public BoardDTO getOne(Long bno) {
-        
-        Optional<Board> result = boardRepository.findById(bno);
-
-        Board board = result.orElseThrow();
-
-        BoardDTO dto = modelMapper.map(board, BoardDTO.class);
-
-        return dto;
-
-    }
+public class BoardSearchImpl {
     
 }
