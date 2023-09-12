d744d84a7324a705afac9d1737b0f0d5536adb95
tarkIlJun <tack89826@gmail.com>
2023-09-12 16:39:41 +0900

    BoardReadDTO

---
+++ src/main/java/tack/project/boot01/dto/BoardReadDTO.java
@@ -0,0 +1,21 @@
+package tack.project.boot01.dto;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public interface BoardReadDTO {
+
+    /////////////////////////////////////////////
+    Long getBno();
+    String gettitle();
+    String getcontent();
+    
+    /////////////////////////////////////////////
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    LocalDateTime getRegDate();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    LocalDateTime getModDate();
+    
+}
--- src/main/java/tack/project/boot01/repository/BoardRepository.java
+++ src/main/java/tack/project/boot01/repository/BoardRepository.java
@@ -1,5 +1,9 @@
 package tack.project.boot01.repository;
 
-public interface BoardRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import tack.project.boot01.domain.Board;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
     
 }
