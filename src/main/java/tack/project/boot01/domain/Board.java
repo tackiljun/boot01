ec7de1be96183f2633414bf2d5e511e64a533032
tarkIlJun <tack89826@gmail.com>
2023-09-12 15:18:32 +0900

    Board-domain

--- src/main/java/tack/project/boot01/domain/Board.java
+++ src/main/java/tack/project/boot01/domain/Board.java
@@ -1,5 +1,46 @@
 package tack.project.boot01.domain;
 
-public class Board {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Table(name = "t_board")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Getter
+public class Board extends BaseEntity {
+
+    ///////////////////////////////////////////////////
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    ///////////////////////////////////////////////////
+    @Column(length = 200, nullable = false)
+    private String title;
+
+    ///////////////////////////////////////////////////
+    @Column(length = 1000, nullable = false)
+    private String contents;
+
+    ///////////////////////////////////////////////////
+    @Column(length = 50, nullable = false)
+    private String writer;
+
+    ///////////////////////////////////////////////////
+    public void changeTitle(String title) {
+        this.title = title;
+    }
     
 }
