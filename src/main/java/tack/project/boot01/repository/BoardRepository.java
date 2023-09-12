1b5ee6267a937c3681050af4d21cbab50a5c4aaa
tarkIlJun <tack89826@gmail.com>
2023-09-12 16:43:33 +0900

    BoardRepository

--- src/main/java/tack/project/boot01/repository/BoardRepository.java
+++ src/main/java/tack/project/boot01/repository/BoardRepository.java
@@ -1,9 +1,52 @@
 package tack.project.boot01.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import tack.project.boot01.domain.Board;
+import tack.project.boot01.dto.BoardReadDTO;
+import tack.project.boot01.repository.search.BoardSearch;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
+
+    ////////////////////////////////////////////////////////////////////////////////
+    @Query("select b from Board b where b.bno = :bno")
+    BoardReadDTO readOne(@Param("bno") Long bno);
+    
+    List<Board> findByTitleContaining(String title);
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Query("select b from Board b where b.title like %:title% ")
+    List<Board> listTitle(@Param("title") String title);
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Query("select b.bno, b.title from Board b where b.title like %:title% ")
+    List<Object[]> listTitle2(@Param("title") String title);
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Query("select b.bno, b.title from Board b where b.title like %:title% ")
+    Page<Object[]> listTitle2(@Param("title") String title, Pageable pageable);
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Modifying
+    @Query("update Board b set b.title = :title where b.bno = :bno ")
+    int modifyTitle(@Param("title") String title, @Param("bno") Long bno);
+
+    Page<Board> findByContentsContaining(String contents, Pageable pageable);
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Query(value = "select * from t_board ", nativeQuery = true)
+    List<Object[]> listNative();
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Query(
+        "select b.bno, b.title, b.writer, count(r), b.regDate, b.modDate from Board b left outer join Reply r on r.board = b group by b order by b.bno desc")
+    List<Object[]> getListWithRcnt();
     
 }
