a19984488b7bfcceb65b40f0f514d6581874b4e8
tarkIlJun <tack89826@gmail.com>
2023-09-13 20:01:00 +0900

    Reply

--- src/main/java/tack/project/boot01/domain/Reply.java
+++ src/main/java/tack/project/boot01/domain/Reply.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
