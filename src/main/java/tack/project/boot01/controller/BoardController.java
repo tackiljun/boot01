63ab1aeffdd122e07371b0596065b256a5484468
tarkIlJun <tack89826@gmail.com>
2023-09-13 19:58:32 +0900

    BoardController

--- src/main/java/tack/project/boot01/controller/BoardController.java
+++ src/main/java/tack/project/boot01/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
 import tack.project.boot01.dto.BoardDTO;
 import tack.project.boot01.dto.BoardListRcntDTO;
 import tack.project.boot01.dto.PageRequestDTO;
