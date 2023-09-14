750e957814fae71b090a62ea29d647d936360850
tarkIlJun <tack89826@gmail.com>
2023-09-14 14:50:20 +0900

    Config

---
+++ src/main/java/tack/project/boot01/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package tack.project.boot01.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+
+
+@OpenAPIDefinition(info = @Info(title = "Zerock App",version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+        .group("Zerock OPEN API v1")
+        .pathsToMatch(paths)
+        .build();
+
+    }
+    
+}
