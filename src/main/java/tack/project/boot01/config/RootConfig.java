c0f7ebe03b38c5d2fa17309204fa8f62061e4ef3
tarkIlJun <tack89826@gmail.com>
2023-10-31 14:38:56 +0900

    RootConfig

--- src/main/java/tack/project/boot01/config/RootConfig.java
+++ src/main/java/tack/project/boot01/config/RootConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RootConfig {
 
+    ///////////////////////////////////////////////////////////////////////////////////
     @Bean
     public ModelMapper getMapper() {
 
