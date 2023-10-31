d21ac0717fee0bc34e20dddb3be3719365b4a40f
tarkIlJun <tack89826@gmail.com>
2023-10-31 17:44:13 +0900

    ReplyPageRequestDTO

--- src/main/java/tack/project/boot01/dto/ReplyPageRequestDTO.java
+++ src/main/java/tack/project/boot01/dto/ReplyPageRequestDTO.java
@@ -2,10 +2,12 @@ package tack.project.boot01.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 
+@Data
 @Builder
 @ToString
 @NoArgsConstructor
