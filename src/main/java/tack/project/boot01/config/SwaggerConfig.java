a2844086b094d744f330626b473553182f33f8d5
tarkIlJun <tack89826@gmail.com>
2023-10-31 14:39:43 +0900

    SwaggerConfig

--- src/main/java/tack/project/boot01/config/SwaggerConfig.java
+++ src/main/java/tack/project/boot01/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 public class SwaggerConfig {
 
+    //////////////////////////////////////
     @Bean
     public GroupedOpenApi chatOpenApi() {
 
