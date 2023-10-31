2a8ddbab6f4729c8dac870e302ea3409126ae78a
tarkIlJun <tack89826@gmail.com>
2023-10-31 14:40:18 +0900

    SwaggerConfig

--- src/main/java/tack/project/boot01/config/SwaggerConfig.java
+++ src/main/java/tack/project/boot01/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package tack.project.boot01.config;
 
 import org.springdoc.core.models.GroupedOpenApi;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
