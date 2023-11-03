5f9e4e719fe9988f89aa7138013d7477b850c0cb
tarkIlJun <tack89826@gmail.com>
2023-11-03 17:25:57 +0900

    RootConfig

--- src/main/java/tack/project/boot01/config/RootConfig.java
+++ src/main/java/tack/project/boot01/config/RootConfig.java
@@ -2,6 +2,7 @@ package tack.project.boot01.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
